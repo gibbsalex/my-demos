d57d427a12961143d11249c422af4924b78c43ee
Alex G <146039097+axgib@users.noreply.github.com>
2023-10-31 16:15:23 -0700

    Create ExplorePID_intro.m
    Looking into the relationship between each of the P,I,D components of the controller.

---
+++ ExplorePID_intro.m
@@ -0,0 +1,62 @@
+%% Exploring PID controllers using RR Robotics
+%Example following: http://robotics.ucsd.edu/RR.pdf
+%See section 11.3.1 PID(Proportional-Integral-Derivative) controllers
+clear; clc; close all;
+
+
+K = 1; % Proportionallity Constant
+Ti = 10; %Time constant for integral term. Typically Ti>Td
+Td = 0.01; %TIme constant for the derivative term
+
+%D = K * (1 + 1/(Ti*s) + Td * s)
+
+%PID
+D = K*Td* tf([1 1/Td 1/(Td*Ti)], [1 0] )
+bode(D);% showing exponential integral and derivative action
+hold on
+legend("AutoUpdate","on")
+
+%Proportional Controller
+Dp = tf(K, 1)
+bode(Dp)
+
+%Derivative Controller
+Dd = tf([K 0], [1])
+bode(Dd)
+
+%Integral Controller
+Di = tf([K], [1 0])
+bode(Di)
+
+%Propotional Derivative Controller
+Dpd = Dp + Dd
+bode(Dpd)
+
+%Proportinal Integral Controller
+Dpi = Dp + Di
+bode(Dpi)
+
+%% Changing Ti and Td
+close all;
+
+%Pg 11-18
+%"Taking Ti -> inf and Td -> 0 reduces PID to a P controller
+%Taking Ti -> inf or Td -> 0 reduces PID to a PD or PI
+
+K_1 = [1]; % Proportionallity Constant
+Ti_1 = [10]; %Time constant for integral term. Typically Ti>Td
+Td_1 = 0.01; %TIme constant for the derivative term
+
+K_2 = 1;
+Ti_2 = 100;
+Td_2 = 0.01;
+%D = K * (1 + 1/(Ti*s) + Td * s)
+
+%PID
+D = K_1*Td_1* tf([1 1/Td_1 1/(Td_1*Ti_1)], [1 0] )
+D2 = K_2*Td_2* tf([1 1/Td_2 1/(Td_2*Ti_2)], [1 0] )
+bode(D);% showing exponential integral and derivative action
+hold on; legend("AutoUpdate","on")
+
+bode(D2)
+bode(Dp)
