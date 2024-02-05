8b7856165a356bb2f1b2b5ecb3298e77bc1ee76d
Alex G <146039097+axgib@users.noreply.github.com>
2024-02-05 10:51:45 -0800

    Learning classes and how to use vscode
    Explored the python documentation for classes. Learned about instances, methods, mutablility. Made it to 9.4 so half way through the classes chapter

--- PythonLab/blankbox.py
+++ PythonLab/blankbox.py
@@ -1,13 +1,34 @@
-
+##Following the python documentation
 
 class vehicle():
-    def __init__(self, color, num1):
-        self.color = color
+
+    sharedlist = []
+
+    def __init__(self, color, counter):
+        self.color = color #This is a data attribute
         self.velocity = 0
-        self.num1 = num1
+        self.counter = counter
         self.num2 = 2
+        #self.data = []
     
-    def add(self):
+    def add(self): # Attribute Instance
         global sum
         sum = self.num1 + self.num2
         return sum
+    
+    def add_list(self, item):
+        self.sharedlist.append(item)
+    
+bluecar = vehicle('blue', 5)
+redcar = vehicle('red', 6)
+
+while bluecar.counter < 10:
+    bluecar.counter = bluecar.counter * 2
+    print(bluecar.counter)
+
+bluecar.sharedlist
+redcar.sharedlist
+bluecar.add_list("griddy") # Adds this too all sharedlists
+
+redcar.sharedlist # SHift + enter compiles code on that line
+redcar.sharedlist # Alt+shift+down copy and pastes the entire line
